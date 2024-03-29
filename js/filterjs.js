66e5ddc6d2bae69c127417eb9bffc424b1c20961
medusa-ph <114103760+medusa-ph@users.noreply.github.com>
2024-03-29 11:33:18 +0800

    Update filterjs.js

--- js/filterjs.js
+++ js/filterjs.js
@@ -22,27 +22,6 @@ $(".filter-btn").click(function () {
 
 /*
 $(document).ready(function () {
-  // Hide all items initially
-  $('.filter').hide();
-
-  // Filter button click event
-  $(".filter-btn").click(function () {
-    // Get selected filter options
-    var type = $(".filter-type option:selected").val();
-    var brand = $(".filter-brand option:selected").val();
-
-    // Show items based on selected options
-    if (type !== '0' && brand !== '0') {
-      $('.' + brand + '.' + type).show();
-    } else {
-      // If both filters are not selected, hide all items
-      $('.filter').hide();
-    }
-  });
-});
-*/
-
-$(document).ready(function () {
   // Filter button click event
   $(".filter-btn").click(function () {
     // Hide all items before applying the filter
@@ -62,5 +41,31 @@ $(document).ready(function () {
     }
   });
 });
+*/
+
+$(document).ready(function () {
+  // Hide all items initially
+  $('.filter').hide();
+
+  // Filter button click event
+  $(".filter-btn").click(function () {
+    // Get selected filter options
+    var type = $(".filter-type option:selected").val();
+    var brand = $(".filter-brand option:selected").val();
+
+    // Hide all items before applying the filter
+    $('.filter').hide();
+
+    // Show items based on selected options
+    if (type !== '0' && brand !== '0') {
+      $('.' + brand + '.' + type).show();
+    } else if (type === '0' && brand !== '0') {
+      $('.' + brand).show();
+    } else if (type !== '0' && brand === '0') {
+      $('.' + type).show();
+    }
+  });
+});
+
 
 
